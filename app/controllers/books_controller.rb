705aed7a2569d0037a69cd660286c68315723b3e
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-31 14:28:21 -0700

    dream coding of index
    Co-authored-by: Trevor Nodland <trevor.nodland@gmail.com>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,14 @@
 class BooksController < ApplicationController
   def index
+    @top_three_books = Book.sort_avg_rating(:desc)
+    @worst_three_books = Book.sort_avg_rating(:asc)
+    @top_three_reviewers = User.top_three_reviewers
 
     if params[:sort_by_worst_to_best => true]
 
       @books = Book.sort_avg_rating("asc")
     else
-      @books = Book.all 
-      @top_three_books = Book.sort_avg_rating(:desc)
-      @worst_three_books = Book.sort_avg_rating(:asc)
-      @top_three_reviewers = User.top_three_reviewers
+      @books = Book.all
     end
 
   end
