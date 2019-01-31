6a65ce6ff3283df7e8f4fecdb4099221f228c90a
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-31 13:08:59 -0700

    creates test for method that sorts books by their rating
    Co-authored-by: Trevor Nodland <trevor.nodland@gmail.com>

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -28,4 +28,31 @@ RSpec.describe Book, type: :model do
       expect(book_1.average_rating).to eq(3.67)
     end
   end
+
+  describe 'Class Methods' do
+    describe '.sort_avg_rating' do
+      it 'can sort the books by average rating' do
+        coelho= Author.create(name:'Paulo Coelho')
+        book_1 = coelho.books.create(title:"Good Book", page_count: 200, year: 1988)
+        book_2 = coelho.books.create(title:"Good Book 2", page_count: 150, year: 2000)
+        book_3 = coelho.books.create(title:"Good Book 3", page_count: 150, year: 2000)
+        book_4 = coelho.books.create(title:"Decent Book", page_count: 150, year: 2000)
+
+        user_1 = User.create(username:"User1")
+
+        Review.create(title:"Good review for book 1",review_text: "Loved it", rating: 5, book: book_1, user: user_1 )
+        Review.create(title:"Good review for book 2",review_text: "Loved it", rating: 4, book: book_2, user: user_1 )
+        Review.create(title:"Good review for book 3",review_text: "Loved it", rating: 3, book: book_3, user: user_1 )
+        Review.create(title:"Good review for book 3",review_text: "Loved it", rating: 1, book: book_4, user: user_1 )
+
+
+        worst_books = [book_4, book_3, book_2]
+        best_books = [book_1, book_2, book_3]
+
+        expect(Book.sort_avg_rating("asc")).to eq(worst_books)
+        expect(Book.sort_avg_rating("desc")).to eq(best_books)
+
+      end
+    end
+  end
 end
