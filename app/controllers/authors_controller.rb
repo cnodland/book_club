112c4f27327862688f29400a6d880e2423b0575a
Teresa Knowles <teresa07moreno@gmail.com>
2019-02-04 17:26:40 -0700

    deletes author
    Co-authored-by: Trevor Nodland <trevor.nodland@gmail.com>

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,15 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
+
+  def destroy
+    # binding.pry
+    @author = Author.find(params[:id])
+    # AuthorBook.where(author_id: @author.id).destroy_all
+    @author.books.each do |book|
+      book.destroy
+    end
+    @author.destroy
+    redirect_to books_path
+  end
 end
